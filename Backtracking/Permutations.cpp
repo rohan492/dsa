2ca24c39c54656c64a7463cc5dfe0f7d465e6415
Rohan Mathur <rm30092000mr@gmail.com>
2024-07-12 03:47:57 +0530

    12 July 2024

---
+++ Backtracking/Permutations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void backtrack(vector<int>& nums, int start, vector<vector<int>>& result) {
+        if (start == nums.size()) {
+            result.push_back(nums);
+            return;
+        }
+        for (int i = start; i < nums.size(); i++) {
+            swap(nums[i], nums[start]);
+            backtrack(nums, start + 1, result);
+            swap(nums[i], nums[start]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        backtrack(nums, 0, result);
+        return result;
+    }
+};
