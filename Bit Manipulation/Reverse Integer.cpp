0ad80d2d74f06b94cdc96bc2700d33c24c69a5dc
Rohan Mathur <rm30092000mr@gmail.com>
2024-07-19 14:35:51 +0530

    19 July 2024

---
+++ Bit Manipulation/Reverse Integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while(x != 0) {
+            int lastDigit = x % 10;
+            x /= 10;
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && lastDigit > INT_MAX % 10)) {
+                return 0;
+            }
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && lastDigit < INT_MIN % 10)) {
+                return 0;
+            }
+            rev = rev * 10 + lastDigit;
+        }
+        return rev;
+    }
+};
