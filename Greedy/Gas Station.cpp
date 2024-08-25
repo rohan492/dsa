97c9ca5e1e11a75fcfc27c308142cb65eee12658
Rohan Mathur <rm30092000mr@gmail.com>
2024-08-25 09:14:35 +0530

    25 August 2024

---
+++ Greedy/Gas Station.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int totalGas = 0, totalCost = 0;
+        for (int i = 0; i < n; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+        }
+        if (totalGas < totalCost) return -1;
+        int result = 0, total = 0;
+        for (int i = 0; i < n; i++) {
+            total += gas[i] - cost[i];
+            if (total < 0) {
+                total = 0;
+                result = i + 1;
+            }
+        }
+        return result;
+    }
+};
