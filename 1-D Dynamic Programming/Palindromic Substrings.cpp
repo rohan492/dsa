a699e32e95de4e95d19b119b4bfff0d480ab7cd2
Rohan Mathur <rm30092000mr@gmail.com>
2024-10-19 11:46:52 +0530

    19 October, 2024

--- 1-D Dynamic Programming/Palindromic Substrings.cpp
+++ 1-D Dynamic Programming/Palindromic Substrings.cpp
@@ -1,3 +1,4 @@
+// Top-Down
 class Solution {
 public:
     int t[1001][1001];
@@ -20,6 +21,32 @@ public:
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
                 if (check(s, i, j)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+// Bottom-Up
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        int count = 0;
+        vector<vector<bool>> t(n, vector<bool>(n, false));
+        for (int L = 1; L <= n; L++) {
+            for (int i = 0; i+L-1 < n; i++) {
+                int j = i + L - 1;
+                if (i == j) {
+                    t[i][i] = true;
+                } else if (i+1 == j) {
+                    t[i][j] = (s[i] == s[j]);
+                } else {
+                    t[i][j] = (s[i] == s[j] && t[i+1][j-1]);
+                }
+                if (t[i][j]) {
                     count++;
                 }
             }
